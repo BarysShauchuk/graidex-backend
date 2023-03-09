bb192899a5aaf9c49eff87938c5c26fded252942
BarysShauchuk <borshevchukn@gmail.com>
2023-03-09 22:18:56 +0200

    Implement IRepository interface

---
+++ Graidex.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graidex.Domain.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity GetById(int id);
+        List<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
